5df84344cd24bade27cca475f69c8cab3914e6d9
Mollie Pettit <mollie.marie@gmail.com>
2016-09-20 18:54:21 -0500

    creates list of airports near the destination city, finds the price from ORD to each airport, and prints out the cheapest price

--- distance.py
+++ distance.py
@@ -25,7 +25,9 @@ worksheet = spreadsheet.worksheet('Sheet1')
 # a city and country. Build that in.
 # 2) feed in real data from google spreadsheets
 
-start_airports = ['MWD', 'ORD']# start = 'ORD'
+start_airport = 'ORD'# start = 'ORD'
+start_city = "Chicago, IL"
+DRIVING_LIMIT = 18000
 # destinations = ['New York, NY', 'St. Louis, Missouri', 'Stockholm, Sweden', 
 # 	   'Ann Arbor, MI', 'Minneapolis, MN']
 # destinations = ['Chicago, IL']
@@ -44,74 +46,90 @@ gmaps = googlemaps.Client(key='AIzaSyAKHDuZsqZRAwxP9BqVCw-VmMTbeaoSoso')
 # geocode_result = gmaps.geocode('Chicago, IL')
 
 for row_number, row  in enumerate(utils.iter_worksheet(spreadsheet, 'Sheet1', header_row = 1)):
-	start_date = (parse(row['start date'])).strftime('%Y-%m-%d')
-	end_date = (parse(row['end date'])).strftime('%Y-%m-%d')
-	destination = row['city'] + ', ' + row['country']
-	geocode = gmaps.geocode(destination)
-	lat = geocode[0]['geometry']['bounds']['southwest']['lat']
-	lng = geocode[0]['geometry']['bounds']['southwest']['lng']
-	destination_airports = [k for _, k in sorted(geo_a.findNearPoint((lat, lng), 40))]
-
-	# loop through all airports retrieved in order to get the cheapest cost
-
-	pdb.set_trace()
-
-	# if destination == start:
-	# 	# if destination city is same as start city
-	# 	price = 0
-	# else:
-	distance = gmaps.distance_matrix(start, destination)
-	if distance['rows'][0]['elements'][0]['status'] == 'ZERO_RESULTS':
-		# this means the destination is not driveable
-		print(destination + ': not driveable')
+	status = row['status']
+	if status == 'past':
+		continue
 	else:
-		dur = distance['rows'][0]['elements'][0]['duration']['text']
-		dist = distance['rows'][0]['elements'][0]['distance']['text'][0:-3]
-		if dist == '':
-			continue
-		dist_int = int(dist.replace(',', ''))
-		price_drive = round(((dist_int * gas_price) / (mpg * km_in_mile)), 2)
-		time = dur.replace(' hours ', ':').replace('hour', ':').replace(' mins', '').replace(' min', '').split(':')
-		if len(time) == 1:
-			dt_seconds = datetime.timedelta(minutes=int(time[0])).seconds
-		elif len(time) == 2:
-			dt_seconds = datetime.timedelta(hours=int(time[0]), minutes=int(time[1])).seconds
-		print(destination + ': '+ dur + ', $' + str(price_drive))
-	if dt_seconds < 18000: 
-		#less than 18000 seconds, or five hours
-		price = price_drive
-	else:
-		# get airline price
-		params = {
-		  "request": {
-		    "slice": [
-		      {
-		        "origin": start,
-		        "destination": destination,
-		        "date": "2016-11-19"
-		      },
-		      {
-		      	"origin": destination,
-		      	"destination": start, 
-		      	"date": "2016-11-25"
-		      }
-		    ],
-		    "passengers": {
-		      "adultCount": 1
-		    },
-		    "solutions": 1,
-		    "refundable": False
-		  }
-		}
+		start_date = (parse(row['start date'])).strftime('%Y-%m-%d')
+		end_date = (parse(row['end date'])).strftime('%Y-%m-%d')
+		destination = row['city'] + ', ' + row['country']
+		geocode = gmaps.geocode(destination)
+		lat = geocode[0]['geometry']['bounds']['southwest']['lat']
+		lng = geocode[0]['geometry']['bounds']['southwest']['lng']
+		destination_airports = [k for _, k in sorted(geo_a.findNearPoint((lat, lng), 40))]
 
-		response = requests.post(url, data=json.dumps(params), headers=headers)
-		data = response.json()
+		# loop through all airports retrieved in order to get the cheapest cost
 
-		price_string = data['trips']['tripOption'][0]['saleTotal']
-		price_split = re.split('(\d+)', price_string)
-		price = price_split[1]
-		currency = price_split[0]
-		print(destination + ', $' + str(price))
+		# pdb.set_trace()
+
+		# if destination == start:
+		# 	# if destination city is same as start city
+		# 	price = 0
+		# else:
+		distance = gmaps.distance_matrix(start_city, destination)
+		if distance['rows'][0]['elements'][0]['status'] == 'ZERO_RESULTS':
+			# this means the destination is not driveable
+			print(destination + ': not driveable')
+			dt_seconds = DRIVING_LIMIT + 1
+		else:
+			dur = distance['rows'][0]['elements'][0]['duration']['text']
+			dist = distance['rows'][0]['elements'][0]['distance']['text'][0:-3]
+			if dist == '':
+				continue
+			dist_int = int(dist.replace(',', ''))
+			price_drive = round(((dist_int * gas_price) / (mpg * km_in_mile)), 2)
+			if 'day' in dur:
+				dt_seconds = DRIVING_LIMIT + 1
+			else:
+				time = dur.replace(' hours ', ':').replace('hour', ':').replace(' mins', '').replace(' min', '').split(':')
+				# pdb.set_trace()
+				if len(time) == 1:
+					dt_seconds = datetime.timedelta(minutes=int(time[0])).seconds
+				elif len(time) == 2:
+					dt_seconds = datetime.timedelta(hours=int(time[0]), minutes=int(time[1])).seconds
+				print(destination + ': '+ dur + ', $' + str(price_drive))
+		if dt_seconds < DRIVING_LIMIT: 
+			#less than 18000 seconds, or five hours
+			price = price_drive
+		else:
+			price = 9999
+			for airport2 in destination_airports:
+			# get airline price
+				params = {
+				  "request": {
+				    "slice": [
+				      {
+				        "origin": start_airport,
+				        "destination": airport2,
+				        "date": start_date
+				      },
+				      {
+				      	"origin": airport2,
+				      	"destination": start_airport, 
+				      	"date": end_date
+				      }
+				    ],
+				    "passengers": {
+				      "adultCount": 1
+				    },
+				    "solutions": 1,
+				    "refundable": False
+				  }
+				}
+
+				response = requests.post(url, data=json.dumps(params), headers=headers)
+				data = response.json()
+				try:
+					price_string = data['trips']['tripOption'][0]['saleTotal']
+					price_split = re.split('(\d+)', price_string)
+					price_test = int(price_split[1])
+					currency = price_split[0]
+					if price_test < price:
+						price = price_test
+				except KeyError:
+					continue
+
+			print(destination + ', $' + str(price))
 
 
-pdb.set_trace()
+		# pdb.set_trace()
