e326b09bf82f3c42adf5ceb4d002b348d2ec0764
Mollie Pettit <mollie.marie@gmail.com>
2016-10-25 14:39:54 -0500

    added attributes and started energy function

--- event.py
+++ event.py
@@ -4,7 +4,8 @@ class Event(object):
 	def __init__(self, name=None, start_date=None, end_date=None, country=None, 
 				 city=None, state=None, url=None, dance_styles=None,
 				 details=None, teachers=None, bands=None, status=None, key=None,
-				 obsolete=None):
+				 obsolete=None, workshop_cost=None, party_pass_cost=None, currency=None, 
+				 distance=None, driving_time=None, flight_cost=None, event_type=None):
 		self.name = name
 		self.start_date = start_date
 		self.end_date = end_date
@@ -19,6 +20,14 @@ class Event(object):
 		self.status = 'upcoming'
 		self.key = key
 		self.obsolete = 0
+		self.workshop_cost = workshop_cost
+		self.party_pass_cost = party_pass_cost
+		self.currency = currency
+		self.distance = distance
+		self.driving_time = driving_time
+		self.flight_cost = flight_cost
+		self.event_type = event_type
+
 
 
 		def __str__(self):
